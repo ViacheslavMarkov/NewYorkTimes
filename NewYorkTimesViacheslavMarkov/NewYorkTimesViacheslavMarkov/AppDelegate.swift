924c1ff8be6c0ae6a089bf07283777f7363cc05e
Viacheslav Markov <markvyach83@gmail.com>
2023-07-09 21:22:05 +0300

    added Reachability to the Internet

--- NewYorkTimesViacheslavMarkov/NewYorkTimesViacheslavMarkov/AppDelegate.swift
+++ NewYorkTimesViacheslavMarkov/NewYorkTimesViacheslavMarkov/AppDelegate.swift
@@ -7,12 +7,13 @@
 
 import UIKit
 import NYViewModels
+import NYNetworking
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     lazy var window: UIWindow? = .init(frame: UIScreen.main.bounds)
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
@@ -29,5 +30,25 @@ private extension AppDelegate {
         let nav = UINavigationController(rootViewController: vc)
         window?.rootViewController = nav
         window?.makeKeyAndVisible()
+    }
+    
+    func checkInternetConnection() {
+        do {
+            try Network.reachability = Reachability(hostname: "www.google.com")
+        }
+        catch {
+            switch error as? Network.Error {
+            case let .failedToCreateWith(hostname)?:
+                print("Network error:\nFailed to create reachability object With host named:", hostname)
+            case let .failedToInitializeWith(address)?:
+                print("Network error:\nFailed to initialize reachability object With address:", address)
+            case .failedToSetCallout?:
+                print("Network error:\nFailed to set callout")
+            case .failedToSetDispatchQueue?:
+                print("Network error:\nFailed to set DispatchQueue")
+            case .none:
+                print(error)
+            }
+        }
     }
 }
